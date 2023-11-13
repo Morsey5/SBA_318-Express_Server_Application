d744feb5efaa540eac40fd0f9d585021fd8387ba
Morsey5 <temeikamorse@gmail.com>
2023-11-12 22:11:49 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  // Render your to-do list page here
+  res.send('Hello, welcome to your To-Do List!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
