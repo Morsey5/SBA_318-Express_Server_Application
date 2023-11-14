f4fdb10574cb2a6e6e15fe51537dc36fd4f21df1
Morsey5 <temeikamorse@gmail.com>
2023-11-14 01:29:50 -0500

    error, update task.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
@@ -6,22 +7,64 @@ const port = 3000;
 
 // Load users, tasks, and categories from JSON files
 const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'));
-const tasks = JSON.parse(fs.readFileSync('./data/tasks.json', 'utf-8'));
-const categories = JSON.parse(fs.readFileSync('./data/categories.json', 'utf-8'));
+let tasks = JSON.parse(fs.readFileSync('./data/tasks.json', 'utf-8'));
+let categories = JSON.parse(fs.readFileSync('./data/categories.json', 'utf-8'));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(express.static('views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-const indexRoutes = require('./routes/index');
-const taskRoutes = require('./routes/tasks');
-const categoryRoutes = require('./routes/categories');
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/tasks', require('./routes/tasks'));
+app.use('/categories', require('./routes/categories'));
 
-app.use('/', indexRoutes);
-app.use('/tasks', taskRoutes);
-app.use('/categories', categoryRoutes);
+// Handle task form submission
+app.post('/tasks/add', (req, res) => {
+  const newTask = {
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  // Process the form submission, add the task to the tasks array
+  tasks.push(newTask);
+
+  // Redirect to the tasks page or send a success message
+  res.redirect('/tasks');
+});
+
+// Handle category form submission
+app.post('/categories/add', (req, res) => {
+  const newCategory = {
+    name: req.body.name,
+  };
+
+  // Process the form submission, add the category to the categories array
+  categories.push(newCategory);
+
+  // Redirect to the categories page or send a success message
+  res.redirect('/categories');
+});
+
+// RESTful API Routes
+app.get('/api/tasks', (req, res) => {
+  // Return a JSON representation of all tasks
+  res.json(tasks);
+});
+
+app.get('/api/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const task = tasks.find(t => t.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+
+  // Return a JSON representation of the specific task
+  res.json(task);
+});
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.sendFile('tasks.html', { root: 'views' }));
+// GET /tasks - Retrieve all tasks
+router.get('/', (req, res) => {
+  // Return a JSON representation of all tasks
+  res.json(tasks);
+});
 
-router.get('/error', (req, res, next) => {
-  // Intentional error for testing error handling middleware
-  next(new Error('This is a test error.'));
+// tasks - Create a new task
+router.post('/', (req, res) => {
+  const newTask = {
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  // Process the form submission, add the task to the tasks array
+  tasks.push(newTask);
+
+  // Return the newly created task
+  res.status(201).json(newTask);
+});
+
+// GET /Retrieve a specific task
+router.get('/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const task = tasks.find(t => t.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+
+  // Return a JSON representation of the specific task
+  res.json(task);
+});
+
+// PUT /Update a specific task
+router.put('/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const taskIndex = tasks.findIndex(t => t.id === taskId);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+
+  // Update the task
+  tasks[taskIndex] = {
+    id: taskId,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  // Return the updated task
+  res.json(tasks[taskIndex]);
+});
+
+// DELETE / Delete a specific task
+router.delete('/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const taskIndex = tasks.findIndex(t => t.id === taskId);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+
+  // Remove the task from the tasks array
+  const deletedTask = tasks.splice(taskIndex, 1)[0];
+
+  // Return the deleted task
+  res.json(deletedTask);
 });
 
 module.exports = router;
